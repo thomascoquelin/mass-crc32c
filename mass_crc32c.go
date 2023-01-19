766a43ac223d3b64a238b6685eaa130a708676c9
Thomas Coquelin <thomas.coquelin@ukg.com>
2023-01-19 18:26:23 +0100

    Add file size to output and execution summary information at the end

--- mass_crc32c.go
+++ mass_crc32c.go
@@ -12,6 +12,8 @@ import (
 	"path/filepath"
 	"runtime"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 var wg sync.WaitGroup
@@ -20,60 +22,76 @@ var pathQueue chan string
 var readSize int
 var crc32cTable *crc32.Table
 
+var fileCount uint64
+var fileErrorCount uint64
+var directoryErrorCount uint64
+var ignoredFilesCount uint64
+var totalDataComputed uint64
+
 func printErr(path string, err error) {
 	fmt.Fprintf(os.Stderr, "error: '%s': %v\n", path, err)
 }
 
-func CRCReader(reader io.Reader) (string, error) {
+func CRCReader(reader io.Reader) (string, uint64, error) {
 	checksum := crc32.Checksum([]byte(""), crc32cTable)
 	buf := make([]byte, 1024*readSize)
+	fileSize := uint64(0)
 	for {
 		switch n, err := reader.Read(buf); err {
 		case nil:
 			checksum = crc32.Update(checksum, crc32cTable, buf[:n])
+			fileSize += uint64(n)
 		case io.EOF:
 			b := make([]byte, 4)
 			binary.BigEndian.PutUint32(b, checksum)
 			str := base64.StdEncoding.EncodeToString(b)
-			return str, nil
+			return str, fileSize, nil
 		default:
-			return "", err
+			return "", 0, err
 		}
 	}
 }
 
-func fileHandler() error {
-	wg.Add(1)
+func fileHandler() {
 	defer wg.Done()
 	for path := range pathQueue { // consume the messages in the queue
-		file, err := os.Open(path)
-		defer func(file *os.File) {
-			err := file.Close()
-			if err != nil {
-				printErr(path, err)
-			}
-		}(file)
+		err, fileSize, crc := pathToCRC(path)
 		if err != nil {
 			printErr(path, err)
+			atomic.AddUint64(&fileErrorCount, 1)
 			continue
 		}
-		crc, err := CRCReader(file)
-		if err != nil {
-			printErr(path, err)
-			continue
-		}
-		fmt.Printf("%s %s\n", crc, path)
+		fmt.Printf("%s %d %s\n", crc, fileSize, path)
+		atomic.AddUint64(&fileCount, 1)
+		atomic.AddUint64(&totalDataComputed, fileSize)
 	}
-	return nil
+	return
+}
+
+func pathToCRC(path string) (error, uint64, string) {
+	file, err := os.Open(path)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			printErr(path, err)
+		}
+	}(file)
+	if err != nil {
+		return err, 0, ""
+	}
+	crc, fileSize, err := CRCReader(file)
+	return err, fileSize, crc
 }
 
 func walkHandler(path string, info os.FileInfo, err error) error {
 	if err != nil {
-		nodeType := "file"
 		if info.IsDir() {
-			nodeType = "dir"
+			fmt.Fprintf(os.Stderr, "dir error: '%s': %v\n", path, err)
+			atomic.AddUint64(&directoryErrorCount, 1)
+		} else {
+			fmt.Fprintf(os.Stderr, "file error: '%s': %v\n", path, err)
+			atomic.AddUint64(&fileErrorCount, 1)
 		}
-		fmt.Fprintf(os.Stderr, "%s error: '%s': %v\n", nodeType, path, err)
 		return nil
 	}
 	if info.IsDir() {
@@ -82,6 +100,7 @@ func walkHandler(path string, info os.FileInfo, err error) error {
 	}
 	if !info.Mode().IsRegular() {
 		fmt.Fprintf(os.Stderr, "ignoring: %s\n", path)
+		atomic.AddUint64(&ignoredFilesCount, 1)
 		return nil
 	}
 	pathQueue <- path // add a path message to the queue (blocking when queue is full)
@@ -114,8 +133,10 @@ func main() {
 
 	// create the coroutines
 	for i := 1; i < *j; i++ {
+		wg.Add(1)
 		go fileHandler()
 	}
+	startTime := time.Now()
 	for _, arg := range flag.Args() {
 		err := filepath.Walk(arg, walkHandler)
 		if err != nil {
@@ -124,4 +145,25 @@ func main() {
 	}
 	close(pathQueue)
 	wg.Wait()
+	duration := time.Now().Sub(startTime)
+	fmt.Fprintf(
+		os.Stderr,
+		"Summary:\n"+
+			"Files computed: %d\n"+
+			"File errors: %d\n"+
+			"Folder errors: %d\n"+
+			"Ignored files: %d\n"+
+			"Computed data: %dB\n"+
+			"Duration: %s\n"+
+			"Avg file speed: %d/s\n"+
+			"Avg data speed: %dkB/s\n",
+		fileCount,
+		fileErrorCount,
+		directoryErrorCount,
+		ignoredFilesCount,
+		totalDataComputed,
+		duration.String(),
+		int(float64(fileCount)/duration.Seconds()),
+		int(float64(totalDataComputed)/duration.Seconds()),
+	)
 }
